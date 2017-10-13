b01d1d70fa8af428f8e78f821dfd7c6554fc0106
Narathip Harijiratiwong <tholop@gmail.com>
2017-10-13 22:52:41 +0700

    update schema

--- models/project.js
+++ models/project.js
@@ -2,10 +2,11 @@ var mongoose = require('mongoose');
 
 var projectSchema = mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String , required: true},
-  desc: String,
-  price: Number,
-  photo: String,
+  project_name: { type: String , required: true},
+  status: { type: Boolean, default: true },
+  closed_date: Date,
+  create_date: { type: Date, default: Date.now },
+  update_date: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Project', categorySchema, 'project');
+module.exports = mongoose.model('Project', projectSchema, 'project');
--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
-  username: { type: String , required: true},
+  username: { type: String , required: true, index: { unique: true }, lowercase: true, trim: true},
   firstname: String,
   lastname: String,
-  email: { type: String , required: true},
-  password: { type: String , required: true},
+  email: { type: String , required: true, lowercase: true, trim: true},
+  password: { type: String , required: true, trim: true},
   create_date: { type: Date, default: Date.now },
 });
 
